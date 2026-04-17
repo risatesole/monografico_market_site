b2ab677b5c6a61a06c70dda686cff612dbfc6167
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-17 15:22:02 -0400

    move home page template to global template folder

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello user")
+    context = {
+        "name": "My Page",
+    }
+    return render(request, "pages/home/index.html", context)
