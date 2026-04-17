6107663e532c897055636b3e31bad82af2e6ab61
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-17 16:08:11 -0400

    chore: make account application views more compact

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,23 +1,11 @@
 from django.http import HttpResponse
 
-def security(request):
-    return HttpResponse("We take security very seriously")
-
-def signIn(request):
-    return HttpResponse("Sign In page")
-
-def signUp(request):
-    return HttpResponse("sign Up page")
-
-def signOut(request):
-    return HttpResponse("Sign Out page")
+def security(request): return HttpResponse("We take security very seriously")
+def signIn(request): return HttpResponse("Sign In page")
+def signUp(request): return HttpResponse("sign Up page")
+def signOut(request): return HttpResponse("Sign Out page")
 
 # Out of project scope:
-def validateAccount(request):
-    return HttpResponse("validate account view")
-
-def restorePassword(request):
-    return HttpResponse("restore password view")
-
-def changeEmail(request):
-    return HttpResponse("change email view")
+def validateAccount(request): return HttpResponse("validate account view")
+def restorePassword(request): return HttpResponse("restore password view")
+def changeEmail(request): return HttpResponse("change email view")
