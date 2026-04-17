6e0d188898c492b0c9a88fb4e7011b6881c097cf
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-17 17:35:21 -0400

    feat: add templates to account pages
    add templates for signup, signin, signout and signup

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from utils.env import environment
+
+context = {
+    "name": environment["name"],
+}
 
 def security(request): return HttpResponse("We take security very seriously")
-def signIn(request): return HttpResponse("Sign In page")
-def signUp(request): return HttpResponse("sign Up page")
-def signOut(request): return HttpResponse("Sign Out page")
+def signIn(request): return render(request, "pages/account/signin.html", context)
+def signUp(request): return render(request, "pages/account/signup.html", context)
+def signOut(request): return render(request, "pages/account/signout.html", context)
 
 # Out of project scope:
 def validateAccount(request): return HttpResponse("validate account view")
