f9352e0cc9a97da542da7271e3782159a89a0b68
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-18 10:01:38 -0400

    feat(account): customize User admin interface with detailed list display

--- src/applications/account/admin.py
+++ src/applications/account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
+
+
+admin.site.register(User, UserAdmin)
