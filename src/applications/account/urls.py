27cfaaf090ddeac5609882e10a51bbb0230c5810
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-17 22:59:06 -0400

    chore: connect password restore url with its respective view

--- src/applications/account/urls.py
+++ src/applications/account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import security, signIn, signUp, signOut
+from .views import security, signIn, signUp, signOut,restorePassword
 
 urlpatterns = [
     path("security/", security),
     path("signin/", signIn, name="signin"),
     path("signout/",signOut),
     path("signup/", signUp, name="signup"),
+    path("restorepassword", restorePassword,name="restorepassword"),
 ]
