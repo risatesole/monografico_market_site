35fac5c6bdc464f444d0f09092992d0ab8902579
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-18 17:48:42 -0400

    fix: correct parameter name for password_hash in UserEntity constructor

--- src/applications/account/module/domain/entities/user/UserEntity.py
+++ src/applications/account/module/domain/entities/user/UserEntity.py
@@ -1,11 +1,15 @@
 class UserEntity:
-    def __init__(self, user_id, email, passwordhash, first_name, last_name, created_at, updated_at,is_active,is_email_verified):
+    def __init__(self, user_id, email, password_hash, first_name, last_name, created_at, updated_at, is_active, is_email_verified):
         self.user_id = user_id
         self.email = email
-        self.passwordhash = passwordhash  
+        self.password_hash = password_hash  
         self.first_name = first_name
         self.last_name = last_name
         self.is_active = is_active
         self.created_at = created_at
         self.updated_at = updated_at
         self.is_email_verified = is_email_verified
+
+    def get_value(self, key):
+        return getattr(self, key, None)
+    
