8f17ae17deb07ebcc84181b0dfb573c0ac95f59a
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-18 17:08:01 -0400

    refactor(tests): rename test method for consistency, and remove empty space in account admin.py

--- src/applications/account/admin.py
+++ src/applications/account/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import User
 
-
 class UserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'username', 'email', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
 
--- src/applications/home/tests.py
+++ src/applications/home/tests.py
@@ -5,7 +5,7 @@ class HomePageTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_home_page_loads(self):
+    def test_homepageloading(self):
         response = self.client.get(reverse("home"))
         
         self.assertEqual(response.status_code, 200)
