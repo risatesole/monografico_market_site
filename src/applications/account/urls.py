3ff2d07ce3bd958413d161f06d93bcf5181773fa
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-17 23:02:14 -0400

    feat(account): add URL names for auth-related routes
    - Add name attribute to security route
    - Add name attribute to signout route

--- src/applications/account/urls.py
+++ src/applications/account/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import security, signIn, signUp, signOut,restorePassword
 
 urlpatterns = [
-    path("security/", security),
+    path("security/", security, name="security"),
     path("signin/", signIn, name="signin"),
-    path("signout/",signOut),
+    path("signout/",signOut,name= "signout"),
     path("signup/", signUp, name="signup"),
     path("restorepassword", restorePassword,name="restorepassword"),
 ]
