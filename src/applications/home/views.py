76f7ddc88a8227c4acef6ee58313f44324d4f550
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-19 15:02:49 -0400

    add principal pages

--- src/applications/home/views.py
+++ src/applications/home/views.py
@@ -5,3 +5,5 @@ context = {
 }
 
 def home(request): return render(request, "pages/home/index.html", context)
+def dashboard(request): return render(request, "pages/home/dashboard.html", context)
+def provider(request): return render(request, "pages/home/provider.html", context)
