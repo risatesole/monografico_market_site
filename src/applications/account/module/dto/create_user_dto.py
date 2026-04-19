28768f38ed231189a5d93ff33374dede482a4b33
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-18 21:49:25 -0400

    feat: update CreateUserDTO to include first and last name attributes

--- src/applications/account/module/dto/create_user_dto.py
+++ src/applications/account/module/dto/create_user_dto.py
@@ -1,5 +1,6 @@
 class CreateUserDTO:
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, first_name : str, last_name : str , email : str, password : str):
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.email: str = email
+        self.password: str = password
