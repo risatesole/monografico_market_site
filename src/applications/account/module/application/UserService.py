e5ff5baed553f09ab2df80677d05b04f9d887928
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-18 22:22:03 -0400

    refactor: update execute method to accept UserEntity and streamline user validation

--- src/applications/account/module/application/UserService.py
+++ src/applications/account/module/application/UserService.py
@@ -1,30 +1,23 @@
-from ..infrastructure.user_repository import UserRepository
-from ..dto import CreateUserDTO
+from ..infrastructure.user_repository import UserRepository 
+from ..domain.entities.user.UserEntity import UserEntity 
 
 class SignupService:
-
     @staticmethod
-    def execute(user_data: CreateUserDTO):
-        # Validation
-        if not user_data.username:
-            raise ValueError("Username is required")
+    def execute(user: UserEntity):
+        if not user.first_name:
+            raise ValueError("First Name is required")
 
-        if not user_data.email:
+        if not user.email:
             raise ValueError("Email is required")
 
-        if not user_data.password:
+        if not user.password_hash:
             raise ValueError("Password is required")
 
-        # Check duplicates
-        existing_user = UserRepository.get_by_email(user_data.email)
+        existing_user = UserRepository.get_by_email(user)
+
         if existing_user:
             raise ValueError("User with this email already exists")
 
-        # Create user
-        user = UserRepository.create_user(
-            username=user_data.username,
-            email=user_data.email,
-            password=user_data.password
-        )
+        UserRepository.create_user(user)
 
         return user
