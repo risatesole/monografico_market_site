bebe10e3724a6e8c73ca40e4e336daafc1143e7d
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-20 08:54:14 -0400

    feat: check if user is logged in, if so it redirects to home screen

--- src/applications/home/interfaces/signinInterface.py
+++ src/applications/home/interfaces/signinInterface.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 
 def signinInterface(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
@@ -17,4 +20,3 @@ def signinInterface(request):
         })
 
     return render(request, "pages/auth/signin.html")
-
