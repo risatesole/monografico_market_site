d81e42ec4221689f777567869941baa0370b08d3
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-19 20:00:32 -0400

    add signin signout and signup views

--- src/applications/home/urls.py
+++ src/applications/home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home , dashboard, provider
+from .views import home , dashboard, provider, signin_view, signout_view, signup_view
 
 urlpatterns = [
     path("", home, name="home"),
     path("dashboard/", dashboard, name="dashboard"),
     path("provider/", provider, name="provider"), 
+
+
+    path("signin/", signin_view, name="signin"), 
+    path("signout/", signout_view, name="signout"), 
+    path("signup/", signup_view, name="signup"), 
 ]
--- src/applications/home/views.py
+++ src/applications/home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .models import User
+
+
 
 context = {
     "name": "supermercado",
@@ -7,3 +11,58 @@ context = {
 def home(request): return render(request, "pages/home/index.html", context)
 def dashboard(request): return render(request, "pages/home/dashboard.html", context)
 def provider(request): return render(request, "pages/home/provider.html", context)
+
+# ----------------------
+# SIGN UP
+# ----------------------
+def signup_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        # prevent duplicate emails
+        if User.objects.filter(email=email).exists():
+            return render(request, "signup.html", {
+                "error": "Email already exists"
+            })
+
+        user = User.objects.create_user(
+            email=email,
+            password=password,
+            role="customer",
+            status="active"
+        )
+
+        login(request, user)
+        return redirect("home")
+
+    return render(request, "pages/auth/signup.html")
+
+
+# ----------------------
+# SIGN IN
+# ----------------------
+def signin_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+        return render(request, "signin.html", {
+            "error": "Invalid credentials"
+        })
+
+    return render(request, "pages/auth/signin.html")
+
+
+# ----------------------
+# SIGN OUT
+# ----------------------
+def signout_view(request):
+    logout(request)
+    return redirect("home")
