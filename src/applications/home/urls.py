55bc16592b1f85c10fdc209a318a403d4be89c54
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-20 12:24:55 -0400

    refactor: remove dashboard route from URLs and clean up imports

--- src/applications/home/urls.py
+++ src/applications/home/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import home , dashboard, signin_view, signout_view, signup_view, settings_view, provider_view, apply_to_be_provider_view, provider_request_view
+from .views import home , signin_view, signout_view, signup_view, settings_view, provider_view, provider_request_view
 
 urlpatterns = [
     path("", home, name="home"),
-
-    # intranet site
-    path("dashboard/", dashboard, name="dashboard"),
 
     # authentication
     path("signin/", signin_view, name="signin"),
