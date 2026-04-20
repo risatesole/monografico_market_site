de01a32c7d0a5f8c1782dbffb3708a5d1f0cafc2
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-20 12:49:59 -0400

    refactor: reorganize imports and cleanup in views.py for improved structure

--- src/applications/home/views.py
+++ src/applications/home/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
-from .models import User , ProviderApplicationToBeProvider
-from .interfaces.signupInterface import signup_interface
 from .interfaces.signinInterface import signinInterface
+from .interfaces.signupInterface import signup_interface
 from .interfaces.signoutInterface import signoutInterface
 from .interfaces.ProviderInterface import providerInterface
+from .interfaces.ProviderInterface import provider_request_interface
+from .models import User , ProviderApplicationToBeProvider
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 context = {
     "name": "Supermercado Blanco",
@@ -15,8 +16,8 @@ context = {
 def home(request): return render(request, "pages/home/index.html", context)
 def dashboard(request): return render(request, "pages/home/dashboard.html", context)
 def provider(request): return render(request, "pages/home/provider.html", context)
-def signup_view(request): return signup_interface(request)
 def signin_view(request): return signinInterface(request)
+def signup_view(request): return signup_interface(request)
 def signout_view(request): return signoutInterface(request)
 
 def settings_view(request):
@@ -24,9 +25,6 @@ def settings_view(request):
         return redirect("home")
 
     return HttpResponse("this is settings page")
-
-
-from .interfaces.ProviderInterface import provider_request_interface
 
 def provider_view(request):
     return providerInterface(request)
