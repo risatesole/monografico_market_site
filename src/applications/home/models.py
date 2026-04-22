acbb83e73585f98292e0ead5af38860d1a20f899
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-21 22:01:30 -0400

    remove customer model

--- src/applications/home/models.py
+++ src/applications/home/models.py
@@ -1,3 +1,2 @@
 from .core.models.model_user import User as User
 from .core.models.model_supplier_request import supplier_request as supplier_request
-from .core.models.model_customer import Customer as customer
