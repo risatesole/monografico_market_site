a423c9b9751561ae3b36150487e6543757b913f6
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-21 21:43:26 -0400

    feat: add Customer model and integrate it into home models

--- src/applications/home/models.py
+++ src/applications/home/models.py
@@ -1,2 +1,3 @@
 from .core.models.model_user import User as User
 from .core.models.model_supplier_request import supplier_request as supplier_request
+from .core.models.model_customer import Customer as customer
