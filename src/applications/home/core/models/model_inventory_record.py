b7dd7220bc1811d66090fe4f7f558283a6fd0c73
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-22 06:19:35 -0400

    update inventory records model

--- src/applications/home/core/models/model_inventory_record.py
+++ src/applications/home/core/models/model_inventory_record.py
@@ -2,34 +2,32 @@ from django.db import models
 from .model_product import product
 from ...models import User
 
-class inventory_record(models.Model):
-    product_id = models.ForeignKey(
+class inventory_records(models.Model):
+    product = models.ForeignKey(
         product,
         on_delete=models.CASCADE,
-        db_column="product_id"
+        related_name="inventory_records"
     )
-    recived_at = models.DateTimeField()
+
     provider = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="provided_lots",
-        db_column="provider_id"
+        limit_choices_to={"role": "provider"}
     )
 
     approved_by = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
         null=True,
+        blank=True,
         related_name="approved_lots",
-        db_column="approved_by",
-        limit_choices_to={"role": "internal_user"}
+        limit_choices_to={"role": "employee"}
     )
-
+    quantity = models.PositiveIntegerField()
     acquisition_cost_per_unit = models.FloatField()
     acquisition_tax = models.FloatField()
-
-    received_at = models.DateTimeField()
+    received_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Lot {self.id}"
-
+        return f"Lot {self.id} - {self.product.name}"
