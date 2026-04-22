cf70afe62bfe244b8ed174548eeb95ee3fd1835b
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-22 09:20:13 -0400

    add category field to Product model

--- src/applications/home/core/models/model_product.py
+++ src/applications/home/core/models/model_product.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ("customer", "Customer"),
+        ("employee", "Employee"),
+        ("provider", "Provider"),
+    ]
     name = models.CharField(max_length=255)
     description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
 
     def __str__(self):
         return self.name
