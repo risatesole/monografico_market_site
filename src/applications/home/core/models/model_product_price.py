682c5fe2b61fa7b41d0ccfc66caa6612c77e50ac
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-22 11:54:58 -0400

    feat: add set_by and created at fields in productprice model

--- src/applications/home/core/models/model_product_price.py
+++ src/applications/home/core/models/model_product_price.py
@@ -1,9 +1,12 @@
 from django.db import models
 from .model_product import Product
+from .model_user import User
 
 class ProductPrice(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    set_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.product} - {self.price}"
