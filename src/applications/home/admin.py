ce77686fb04f1c8e648ddb1cb155199a645cd5b0
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-21 20:06:02 -0400

    removed everything in admin.py and created migration

--- src/applications/home/admin.py
+++ src/applications/home/admin.py
@@ -1,83 +1,83 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import *
-
-
-# ----------------------
-# USER ADMIN
-# ----------------------
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    list_display = ("id", "email", "first_name", "last_name", "role", "status")
-    list_filter = ("role", "status")
-
-    search_fields = ("email", "first_name", "last_name")
-    ordering = ("id",)
-
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Personal Info", {"fields": ("first_name", "last_name")}),
-        ("Role & Status", {"fields": ("role", "status")}),
-        ("Permissions", {"fields": ("is_staff", "is_superuser", "groups", "user_permissions")}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "username", "password1", "password2", "role", "status"),
-        }),
-    )
+# from .models import *
 
 
-# ----------------------
-# OTHER MODELS
-# ----------------------
-@admin.register(CustomerOrder)
-class CustomerOrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "customer", "status", "payment_method", "created_at")
-    list_filter = ("status",)
-    search_fields = ("customer__email",)
+# # ----------------------
+# # USER ADMIN
+# # ----------------------
+# @admin.register(User)
+# class UserAdmin(BaseUserAdmin):
+#     list_display = ("id", "email", "first_name", "last_name", "role", "status")
+#     list_filter = ("role", "status")
+
+#     search_fields = ("email", "first_name", "last_name")
+#     ordering = ("id",)
+
+#     fieldsets = (
+#         (None, {"fields": ("email", "password")}),
+#         ("Personal Info", {"fields": ("first_name", "last_name")}),
+#         ("Role & Status", {"fields": ("role", "status")}),
+#         ("Permissions", {"fields": ("is_staff", "is_superuser", "groups", "user_permissions")}),
+#     )
+
+#     add_fieldsets = (
+#         (None, {
+#             "classes": ("wide",),
+#             "fields": ("email", "username", "password1", "password2", "role", "status"),
+#         }),
+#     )
 
 
-@admin.register(CustomerOrderItem)
-class CustomerOrderItemAdmin(admin.ModelAdmin):
-    list_display = ("id", "created_at")
+# # ----------------------
+# # OTHER MODELS
+# # ----------------------
+# @admin.register(CustomerOrder)
+# class CustomerOrderAdmin(admin.ModelAdmin):
+#     list_display = ("id", "customer", "status", "payment_method", "created_at")
+#     list_filter = ("status",)
+#     search_fields = ("customer__email",)
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    search_fields = ("name",)
+# @admin.register(CustomerOrderItem)
+# class CustomerOrderItemAdmin(admin.ModelAdmin):
+#     list_display = ("id", "created_at")
 
 
-@admin.register(ProductLot)
-class ProductLotAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "product",
-        "provider",
-        "approved_by",
-        "acquisition_cost_per_unit",
-        "received_at",
-    )
-
-    search_fields = ("product__name", "provider__email")
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ("id", "name")
+#     search_fields = ("name",)
 
 
-@admin.register(ProductPurchaseRequisition)
-class ProductPurchaseRequisitionAdmin(admin.ModelAdmin):
-    list_display = ("id", "provider", "status", "total_estimated", "requested_at")
-    list_filter = ("status",)
+# @admin.register(ProductLot)
+# class ProductLotAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "id",
+#         "product",
+#         "provider",
+#         "approved_by",
+#         "acquisition_cost_per_unit",
+#         "received_at",
+#     )
+
+#     search_fields = ("product__name", "provider__email")
 
 
-@admin.register(MetaRequestBeProviderAccount)
-class MetaRequestBeProviderAccountAdmin(admin.ModelAdmin):
-    list_display = ("id", "requested_by", "approved_by", "requested_at")
+# @admin.register(ProductPurchaseRequisition)
+# class ProductPurchaseRequisitionAdmin(admin.ModelAdmin):
+#     list_display = ("id", "provider", "status", "total_estimated", "requested_at")
+#     list_filter = ("status",)
 
 
-@admin.register(ProviderApplicationToBeProvider)
-class ProviderApplicationAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "status", "external_id", "created_at")
-    list_filter = ("status",)
-    search_fields = ("user__email",)
+# @admin.register(MetaRequestBeProviderAccount)
+# class MetaRequestBeProviderAccountAdmin(admin.ModelAdmin):
+#     list_display = ("id", "requested_by", "approved_by", "requested_at")
+
+
+# @admin.register(ProviderApplicationToBeProvider)
+# class ProviderApplicationAdmin(admin.ModelAdmin):
+#     list_display = ("id", "user", "status", "external_id", "created_at")
+#     list_filter = ("status",)
+#     search_fields = ("user__email",)
