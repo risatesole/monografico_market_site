f99aa963cec7d7d6bf5a8e6c6e1c29eef3c55837
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-22 12:06:57 -0400

    feat: implement UserService with skeleton methods

---
+++ src/applications/home/core/services/user_service/service_user.py
@@ -0,0 +1,32 @@
+
+class UserService:
+    def deactivateUser(self):
+        raise NotImplementedError("This method is not implemented")
+    
+    def activateUser(self):
+        raise NotImplementedError("This method is not implemented")
+    
+    def createUser(self):
+        raise NotImplementedError("This method is not implemented")
+    
+    def setUserRoleCustomer(self,userid):
+        raise NotImplementedError("This method is not implemented")
+
+    
+    def setUserRoleEmployee(self):
+        raise NotImplementedError("This method is not implemented")
+    
+    def setUserRoleProvider(self):
+        raise NotImplementedError("This method is not implemented")
+    
+    def getUserRole(self,userid):
+        raise NotImplementedError("This method is not implemented")
+    
+    def isUserEmployee(self,userid):
+        raise NotImplementedError("This method is not implemented")
+    
+    def isUserCustomer(self,userid):
+        raise NotImplementedError("This method is not implemented")
+    
+    def isUserProvider(self,userid):
+        raise NotImplementedError("This method is not implemented")
