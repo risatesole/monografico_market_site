ba26eb90e4e95345d000c94d0b4990f964ab7fb3
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-22 09:38:19 -0400

    feat: add UnitSellingRecordsUnits model with relationships to Unit and Batch

---
+++ src/applications/home/core/models/model_selling_record_unit.py
@@ -0,0 +1,25 @@
+from django.db import models
+from model_batch import Batch
+from model_unit import Unit
+
+class UnitSellingRecordsUnits(models.Model):
+    ENTITY_CHOISES = [
+        ("customer", "Customer")
+    ]
+    datetime = models.DateTimeField()
+    unitid = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE,
+        related_name="unit"
+    )
+    batchid = models.ForeignKey(
+        Batch,
+        on_delete=models.CASCADE,
+        related_name="batch"
+    )
+    entity = models.CharField(max_length=20, choices=ENTITY_CHOISES)
+    quantity_of_units = models.PositiveIntegerField()
+    purchase_price = models.FloatField()
+
+    def __str__(self):
+        return None
