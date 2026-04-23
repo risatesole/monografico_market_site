8fd210abc1ca172743a816375e182ac034522b4b
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-22 21:43:08 -0400

    fix: make main be main

--- src/applications/main/apps.py
+++ src/applications/main/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class DuckConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.applications.duck'
+    name = 'src.applications.main'
