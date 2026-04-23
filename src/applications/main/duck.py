f29f5b1e7dd153c73a37eaf56523522479c6a180
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-23 12:27:45 -0400

    add pydoc explaining in submit_product_sale_request

--- src/applications/main/duck.py
+++ src/applications/main/duck.py
@@ -48,6 +48,7 @@ class ProductService:
 
 
 def submit_product_sale_request(providerid,productid,quantity,price):
+    """Submit a provider's request to sell a product for later approval or rejection."""
     provider_service = ProviderService()
     provider_service.setRequestSellProduct(providerid,productid,quantity,price)
 
