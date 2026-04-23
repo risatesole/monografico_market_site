a84819c7134f136c4ed2b3271c1069beeda737c5
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-23 09:41:17 -0400

    add debug function

--- src/applications/main/views.py
+++ src/applications/main/views.py
@@ -3,11 +3,27 @@ from django.shortcuts import render, redirect
 # Shared variable
 shared_messages = []
 
+
+
+
+
+def debug(request):
+    print("=================================================")
+    print("Message:")
+    print(request.POST.get("message"), flush=True)
+    print("=================================================")
+
+
+
+
 def shared_view(request):
     global shared_messages
 
     if request.method == "POST":
         msg = request.POST.get("message")
+        
+        debug(request)
+
         if msg:
             shared_messages.append(msg)
         return redirect("shared")
