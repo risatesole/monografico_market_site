7ae188cbc62f7f5f209a1c380af707b96382fc11
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-24 10:01:49 -0400

    feat: add signin view in main view

---
+++ src/applications/main/logic/views/web/view_web_signin.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from ....models import User
+
+def signin_view(request):
+    error = None
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        # Django does NOT authenticate by email by default
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            print(f"Debug: User doesnt exists")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            error = "Invalid email or password"
+
+    return render(request, "pages/auth/signin.html", {"error": error})
--- src/applications/main/urls.py
+++ src/applications/main/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from .logic.views.web.view_web_provider import provider_view
 from .logic.views.web.view_web_employee import employee_view
 from .logic.views.web.view_web_signup import signup_view
+from .logic.views.web.view_web_signin import signin_view
 
 def home_view(request):
     return HttpResponse("Welcome home - Django is running")
@@ -14,6 +15,7 @@ def home_view(request):
 urlpatterns = [
     path("", home_view, name="home"),
     path("employee/",employee_view, name="employee"),
+    path("signin/",signin_view,name="signin"),
     path("signup/", signup_view, name="signup"),
     path("provider/", provider_view, name="provider"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add this to the end of ] so it saves to the storage
