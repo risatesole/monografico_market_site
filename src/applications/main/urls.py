9bae085f423c412792d6bd3ebba146c78c54a1f2
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-24 07:28:27 -0400

    refactor: move employee related logic from duck to its respective files and folders

--- src/applications/main/urls.py
+++ src/applications/main/urls.py
@@ -1,5 +1,6 @@
 from .logic.views.web.view_web_provider import provider_view
-from .duck import duck_view
+from .logic.views.web.view_web_employee import employee_view
+
 from django.urls import path
 # from django.shortcuts import render
 
@@ -45,6 +46,6 @@ def example_view(request):
     })
 
 urlpatterns = [
-    path("",duck_view, name="employee"),
+    path("employee/",employee_view, name="employee"),
     path("provider/", provider_view, name="provider"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add this to the end of ] so it saves to the storage
