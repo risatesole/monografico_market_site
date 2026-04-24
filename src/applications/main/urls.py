76a6d2bae8fab65bcaa38728a2192f22425208f8
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-24 08:15:29 -0400

    chore: clean urls.py in main application

--- src/applications/main/urls.py
+++ src/applications/main/urls.py
@@ -1,56 +1,15 @@
+from django.shortcuts import render, redirect
+from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.conf import settings
+from django.urls import path
+
 from .logic.views.web.view_web_provider import provider_view
 from .logic.views.web.view_web_employee import employee_view
-
-from django.urls import path
-# from django.shortcuts import render
-
-from django.shortcuts import render, redirect
 from .models import Product
-
-from django.conf import settings
-from django.conf.urls.static import static
-# from .views import  provider_view
-
-def example_view(request):
-    if request.method == "POST":
-
-        # DELETE
-        if "delete_id" in request.POST:
-            product = Product.objects.get(id=request.POST.get("delete_id"))
-            product.delete()
-            return redirect("/duck/")
-
-        # EDIT
-        if "edit_id" in request.POST:
-            product = Product.objects.get(id=request.POST.get("edit_id"))
-            new_name = request.POST.get("new_name")
-
-            if new_name:
-                product.name = new_name
-                product.save()
-
-            return redirect("/duck/")
-
-        # CREATE
-        name = request.POST.get("name")
-        image = request.FILES.get("image")
-
-        if name and image:
-            Product.objects.create(name=name, image=image)
-            return redirect("home")
-
-    products = Product.objects.all()
-
-    return render(request, "duck/page.html", {
-        "products": products
-    })
-
-
-from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("Welcome home - Django is running")
-
 
 from .duck import duck
 urlpatterns = [
