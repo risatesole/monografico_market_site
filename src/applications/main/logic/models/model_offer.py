a92e78f1eb10e310d4143b954fc214d529af1ac4
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-24 17:05:54 -0400

    chore: remove commented code in offers model

--- src/applications/main/logic/models/model_offer.py
+++ src/applications/main/logic/models/model_offer.py
@@ -2,28 +2,6 @@ from django.db import models
 from .model_product import Product
 
 # todo: connect user model as forign key
-# class Offer(models.Model):
-#     STATUS_CHOICE = [
-#         ("PENDING","Pending" ),
-#         ("ACCEPTED","Acepted"),
-#         ("DECLINED","Declined")
-#     ]
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     providerid = models.IntegerField()
-#     datetime = models.DateTimeField()
-#     priceperunit = models.FloatField()
-#     unitquantity = models.IntegerField()
-#     status = models.CharField(max_length=10, choices=STATUS_CHOICE, default="PENDING")
-
-
-
-
-
-
-from django.db import models
-from .model_product import Product
-
-# todo: connect user model as forign key
 class Offer(models.Model):
     STATUS_CHOICE = [
         ("PENDING","Pending" ),
