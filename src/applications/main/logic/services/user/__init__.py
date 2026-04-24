363aca87b00aeb83340a42b481eec28b10341e8a
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-24 08:25:34 -0400

    feat: refactor signup logic into separate view and update routing

--- src/applications/main/logic/services/user/__init__.py
+++ src/applications/main/logic/services/user/__init__.py
@@ -1,1 +1,2 @@
 from .user_service import UserService as UserService
+from .user_service import emailExistsError as emailExistsError 
--- src/applications/main/urls.py
+++ src/applications/main/urls.py
@@ -4,17 +4,18 @@ from django.http import HttpResponse
 from django.conf import settings
 from django.urls import path
 
+from .models import Product
+
 from .logic.views.web.view_web_provider import provider_view
 from .logic.views.web.view_web_employee import employee_view
-from .models import Product
+from .logic.views.web.view_web_signup import signup_view
 
 def home_view(request):
     return HttpResponse("Welcome home - Django is running")
 
-from .duck import duck
 urlpatterns = [
     path("", home_view, name="home"),
     path("employee/",employee_view, name="employee"),
-    path("signup/", duck, name="signup"),
+    path("signup/", signup_view, name="signup"),
     path("provider/", provider_view, name="provider"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add this to the end of ] so it saves to the storage
