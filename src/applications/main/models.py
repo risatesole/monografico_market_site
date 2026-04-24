cbe45ed2c5dc320210371fcd4a15514a2726a92b
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-24 12:34:14 -0400

    refactor: move offer model to its own folder in logic models folder in main application

--- src/applications/main/models.py
+++ src/applications/main/models.py
@@ -1,17 +1,4 @@
 from .logic.models.model_user import User  as User
 from .logic.models.model_user import UserRoles as UserRoles
 from .logic.models.model_product import Product as Product 
-from django.db import models
-class Offer(models.Model):
-    STATUS_CHOICE = [
-        ("PENDING","Pending" ),
-        ("ACCEPTED","Acepted"),
-        ("DECLINED","Declined")
-    ]
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    providerid = models.IntegerField()
-    datetime = models.DateTimeField()
-    priceperunit = models.FloatField()
-    unitquantity = models.IntegerField()
-    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default="PENDING")
-
+from .logic.models.model_offer import Offer as Offer
