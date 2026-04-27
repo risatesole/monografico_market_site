8d49eea3d236fa11c085a58df88f9f5a02980831
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-27 19:09:11 -0400

    chore: remove unused chore init python file in handler folder  for main application

--- src/applications/main/modules/handler/__init__.py
+++
@@ -1,4 +0,0 @@
-# from ..usecases.submit_product_sale_request import submit_product_sale_request
-# from ..usecases.get_product_sale_requests import get_product_sale_requests
-# from ..usecases.get_all_product_sale_requests import get_all_product_sale_requests
-# from .setOfferStatus import setOfferStatus as setOfferStatus
