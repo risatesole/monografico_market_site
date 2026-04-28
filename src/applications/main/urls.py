7d0dcd7ea6ab307f438298b0e06214d5a95ec310
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-28 08:36:22 -0400

    refactor: remove provider view and related URL mapping

--- src/applications/main/urls.py
+++ src/applications/main/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
 
-from .modules.views.web.view_web_provider import provider_view
 from .modules.backoffice.views.backoffice_view import backoffice_view
 from .modules.account.user.views import signin_view, signout_view, signup_view
 
@@ -17,5 +16,4 @@ urlpatterns = [
     path("signin/",signin_view,name="signin"),
     path("signup/", signup_view, name="signup"),
     path("signout/", signout_view, name="signout"),
-    path("provider/", provider_view, name="provider"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add this to the end of ] so it saves to the storage
