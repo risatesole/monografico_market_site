fd38293c7769b60cae0f2d5b5df2d1d965966d34
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-28 07:26:23 -0400

    refactor: remove delete get_all product sale request(offers) usecase

--- src/applications/main/modules/backoffice/views/contexts/backoffice_context_handler.py
+++ src/applications/main/modules/backoffice/views/contexts/backoffice_context_handler.py
@@ -1,17 +1,14 @@
-from ....usecases import get_all_product_sale_requests, get_all_products
+from ....usecases import get_all_products
 from ....product import get_product_price
-
 
 def backoffice_view_context_handler():
     """LOAD DATA"""
-    offers = get_all_product_sale_requests()
     products = get_all_products()
     for product in products:
         # inject price to product data
         product.current_price = get_product_price(product) # type: ignore
 
     context = {
-        "providers_offers": offers,
         "products": products
     }
     return context
--- src/applications/main/modules/usecases/get_all_product_sale_requests.py
+++
@@ -1,5 +0,0 @@
-# todo: relocate this method to product module
-from ..services.provider import ProviderService
-def get_all_product_sale_requests():
-    provider_service = ProviderService()
-    return provider_service.getAllOffers()
