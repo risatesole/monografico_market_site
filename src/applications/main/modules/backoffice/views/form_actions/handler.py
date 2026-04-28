8ef69a6a3a025b4648472d6e25e27f90820f18a9
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-28 07:17:40 -0400

    refactor: rename set_product price form action to respective name for clarity

--- src/applications/main/modules/backoffice/views/form_actions/handler.py
+++ src/applications/main/modules/backoffice/views/form_actions/handler.py
@@ -1,9 +1,8 @@
 from .form_action_delete_product import employee_action_delete_product
-from .set_product_price import employee_action_set_product_price
+from .form_action_set_product_price import employee_action_set_product_price
 from .form_action_add_product import employee_action_add_product
 
 def form_actions_handler(request, form_type):
     employee_action_delete_product(request, form_type)
     employee_action_add_product(request, form_type)
     employee_action_set_product_price(request,form_type)
-
