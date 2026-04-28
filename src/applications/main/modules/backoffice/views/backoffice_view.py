bf1ca5e2d90026c858aeb4e10356fbe630066890
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-28 16:22:19 -0400

    refactor: reorder backoffice_view function definition for clarity

--- src/applications/main/modules/backoffice/views/backoffice_view.py
+++ src/applications/main/modules/backoffice/views/backoffice_view.py
@@ -1,17 +1,13 @@
 from .contexts.backoffice_context_handler import backoffice_view_context_handler
 from django.shortcuts import render
 
-def backoffice_view(request):
-    """HANDLE POST ACTIONS"""
-    return render(request, "backoffice/page.html", backoffice_view_context_handler())
-
-
-
-
 from django.shortcuts import render, redirect
 from ...product.models.model_product import Product
 from ...product.models.price_model import Price
 
+def backoffice_view(request):
+    """HANDLE POST ACTIONS"""
+    return render(request, "backoffice/page.html", backoffice_view_context_handler())
 
 def backoffice_create_product_view(request):
     if request.method == "POST":
