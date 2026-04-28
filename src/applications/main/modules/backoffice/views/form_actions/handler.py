37face051ee2d3c106cbeafab2d855fa34ffe499
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-28 07:14:32 -0400

    refactor: rename form action add product to more clar formaction add product name for clearness

--- src/applications/main/modules/backoffice/views/form_actions/handler.py
+++ src/applications/main/modules/backoffice/views/form_actions/handler.py
@@ -1,7 +1,7 @@
 from .set_offer_status import employee_action_set_offer_status
 from .delete_product import employee_action_delete_product
 from .set_product_price import employee_action_set_product_price
-from .add_product import employee_action_add_product
+from .form_action_add_product import employee_action_add_product
 
 def form_actions_handler(request, form_type):
     employee_action_set_offer_status(request, form_type)
