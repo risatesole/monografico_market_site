3ce7da0334a3affc305e04d16090709fb6f37907
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-29 12:59:12 -0400

    feat: add provider section with mock data and update navigation
    refactor removed the prices section because it was redundant with product, and replaced it with provider

--- src/applications/main/modules/backoffice/views/contexts/backoffice_context_handler.py
+++ src/applications/main/modules/backoffice/views/contexts/backoffice_context_handler.py
@@ -27,9 +27,19 @@ def backoffice_view_context_handler():
         latest_price = product.prices.first() # type: ignore
         product.current_price = latest_price.value if latest_price else 0 # type: ignore
 
+
+    mock_provider_data = [{     
+        "name": "proveedor1",
+        "phone_number": "1 (829)-123-4567",
+        "address": "Avenida casa viva",
+        "email": "provider1@provider.com",
+        "description": "vende flores"
+    }]
+
     context = {
         "customers": customers,
         "employees": employees,
+        "providers": mock_provider_data,
         "products": products,
         "inventory": inventory_stock(products),
         "project":{
